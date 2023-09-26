aae2c50f6398a31978e05fe78d385b094a059e8f
ManuelDonofrio <137343173+ManuelDonofrio@users.noreply.github.com>
2023-09-26 00:29:01 -0300

    Bot1.py
    Bot for logging in using the Selenium library in Python.

--- bot1.py
+++ bot1.py
@@ -7,65 +7,58 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 import random
 
-# Inicializar el navegador Firefox
+
+# To initialize the Firefox browser
 options = webdriver.FirefoxOptions()
 options.add_argument("--start-maximized")  # Maximizar la ventana para una apariencia más natural
 options.add_argument("--disable-infobars")  # Deshabilitar las barras de información
 driver = webdriver.Firefox(options=options)
 
-# Simular movimientos del mouse
+# To simulate mouse movements
 def simulate_mouse_movement(element):
     actions = ActionChains(driver)
     actions.move_to_element(element)
     actions.perform()
 
-# Abrir el sitio web
-url = "https://soysocio.bocajuniors.com.ar/"
+# Open the website
+url = "https://page.../"
 driver.get(url)
 
-# Esperar de manera más natural
+# Wait more naturally
 time.sleep(random.uniform(4, 6))
 
-# Encontrar el botón de "Iniciar Sesión" y hacer clic en él
+# Find the 'Log In' button and click it
 inicio_sesion_btn = driver.find_element(By.CSS_SELECTOR, "#loginButton2")
 simulate_mouse_movement(inicio_sesion_btn)  # Simular movimiento del mouse
 inicio_sesion_btn.click()
 
-# Esperar de manera más natural
+# Wait mpre naturally
 time.sleep(random.uniform(2, 4))
 
-# Esperar a que el formulario de inicio de sesión cargue completamente
+# Wait for the login form to load completely
 wait = WebDriverWait(driver, 20)
 email_input = wait.until(EC.visibility_of_element_located((By.ID, "email")))
 
-# Encontrar el campo de email y llenarlo
-email_input.send_keys("donofriomanuel1@gmail.com")
+# Find the email/username field and fill it in
+email_input.send_keys("mail or username")
 
-# Esperar un poco antes de llenar la contraseña
+# Wait a little before to fill password field
 time.sleep(random.uniform(1, 2))
 
-# Encontrar el campo de contraseña y llenarlo
+# Find password field and fill it 
 contrasenia_input = driver.find_element(By.NAME, "j_password")
 contrasenia_input.send_keys("JRoman12")
 
-# Esperar antes de enviar el formulario
+# Wait before submitting the form
 time.sleep(random.uniform(1, 2))
 
-# Encontrar el botón de "ENTRAR" y hacer clic en él
+# Find the 'LOGIN' button and click on it
 entrar_btn = driver.find_element(By.CSS_SELECTOR, "#btnEntrar button")
 simulate_mouse_movement(entrar_btn)  # Simular movimiento del mouse
 entrar_btn.click()
 
-# Esperar a que inicie sesión
+# Wait for the login to complete
 time.sleep(5)
 
-# Navegar a la sección de compras (ajusta el XPath según tu página)
-compras_link = driver.find_element(By.XPATH, "//a[contains(text(), 'Compras')]")
-simulate_mouse_movement(compras_link)  # Simular movimiento del mouse
-compras_link.click()
-
-# Esperar a que la página de compras cargue (ajusta el tiempo según sea necesario)
-time.sleep(5)
-
-# Cerrar el navegador
+# To close the navegator
 driver.quit()
